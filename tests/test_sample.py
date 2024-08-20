3d459aa31897d8f5b066d60c1554a9f699a61740
abhira0 <82208485+abhira0@users.noreply.github.com>
2024-08-20 23:52:56 +0000

    making tests pass 100% to test the badge

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -7,5 +7,5 @@ MY_LIST = [1,2,3]
 def test_passing():
     assert sum_of_list(MY_LIST) == 6
 
-def test_failing():
-    assert sum_of_list(MY_LIST) == 7
+# def test_failing():
+#     assert sum_of_list(MY_LIST) == 7
